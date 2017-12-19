32fa60dd710f0ac0e40849e0b2fcf295b31c7ecf
Alex Dzul <alexexc2@gmail.com>
2017-12-19 12:14:25 -0600

    update gunicorn file

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,7 +2,7 @@ import multiprocessing
 
 num_workers = multiprocessing.cpu_count() * 2 + 1
 command = '/virtualenvs/pythonizame/bin/gunicorn'
-pythonpath = '/var/www_nginx/pythonizame'
+pythonpath = '/var/www/pythonizame'
 bind = '0.0.0.0:8000'
 workers = num_workers
 timeout = 120
