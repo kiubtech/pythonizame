61d5bfdee8d22dc69f5f9db20d5d8deaa8d43600
Alex Dzul <alexexc2@gmail.com>
2018-06-13 21:52:10 -0500

    reinicializamos migraciones y publicamos funcionalidades básicas para el módulo de videos de Pythonízame.

--- pythonizame/apps/videos/urls.py
+++ pythonizame/apps/videos/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from pythonizame.apps.videos.views import IndexView, PlayListVideosView
+from pythonizame.apps.videos.views import IndexView, PlayListVideosView, VideoDetailView
 
 app_name = "videos"
 
@@ -7,14 +7,5 @@ app_name = "videos"
 urlpatterns = [
     url(r'^$', (IndexView.as_view()), name="list"),
     url(r'^(?P<slug>[-_\w]+)/$', (PlayListVideosView.as_view()), name='playlist-detail'),
-    # url(r'^blog/rss/$', LatestEntriesFeed()),
-    # url(r'^post/new/$', (PostNew.as_view()), name='new_post'),
-    # url(r'^author/([-_\w]+)/$', (ByAuthorView.as_view()), name='by_author'),
-    # url(r'^author/([-_\w]+)/page/([0-9]+)/$', ByAuthorView.as_view(), name='by_author_paginated'),
-    # url(r'^category/([-_\w]+)/$', ByCategoryView.as_view(), name='by_category'),
-    # url(r'^category/([-_\w]+)/page/([0-9]+)/$', ByCategoryView.as_view(),
-    #     name='by_category_paginated'),
-    # url(r'^tag/([-_\w]+)/$', ByTagView.as_view(), name='by_tag'),
-    # url(r'^tag/([-_\w]+)/page/([0-9]+)/$', ByTagView.as_view(), name='by_tag_paginated'),
-    # url(r'^(?P<slug>[-_\w]+)/preview/$', PreviewPostDetailView.as_view(), name='post_preview'),
+    url(r'^video-detail/(?P<pk>[-_\w]+)/$', (VideoDetailView.as_view()), name='video-detail'),
 ]
--- pythonizame/apps/videos/views.py
+++ pythonizame/apps/videos/views.py
@@ -70,7 +70,7 @@ class PlayListVideosView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(PlayListVideosView, self).get_context_data(**kwargs)
-        actual_playlist = context['playlist']  # Post consultado
+        actual_playlist = context['playlist']  # Playlist consultado
         # Obtenemos los post relacionados
         cache_categories = cache.get('pythonizame_videos_categories')
         if cache_categories:
@@ -87,4 +87,38 @@ class PlayListVideosView(DetailView):
         )))
         rel_playlist = PlayList.objects.filter(id__in=rel_playlist_id).order_by('?')[:5]
         context['related_playlist'] = rel_playlist
+        return context
+
+
+class VideoDetailView(DetailView):
+    template_name = "video_detail.html"
+    model = Video
+
+    # def get_object(self, queryset=None):
+    #     obj = super(VideoDetailView, self).get_object(queryset)
+    #     if obj.status == 1:  # Solo se muestra si está autorizada la visualización
+    #         return obj
+    #     else:
+    #         raise Http404
+
+    def get_context_data(self, **kwargs):
+        context = super(VideoDetailView, self).get_context_data(**kwargs)
+        obj_video = context['video']  # Video consultado
+        # Obtenemos el video
+        # Obtenemos los post relacionados
+        # cache_categories = cache.get('pythonizame_videos_categories')
+        # if cache_categories:
+        #     queryset = cache_categories
+        # else:
+        #     queryset = VideoCategory.objects.all()
+        #     cache.set('pythonizame_video_categories', queryset, 60 * 5)  # Cache por 15 mins
+        # context['categories'] = queryset
+        # actual_categories_id = list(actual_playlist.categories.all().values_list('id', flat=True))
+        # rel_playlist_id = set(list(PlayList.objects.filter(status=1,
+        #                                                    categories__in=actual_categories_id).exclude(
+        #     id=actual_playlist.id).values_list(
+        #     'id', flat=True
+        # )))
+        # rel_playlist = PlayList.objects.filter(id__in=rel_playlist_id).order_by('?')[:5]
+        # context['related_playlist'] = rel_playlist
         return context
